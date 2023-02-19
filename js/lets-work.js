efe5e93a7eb55250befeb5a459a926f54111e415
T R Methu <fmethubd@gmail.com>
2023-02-19 14:52:28 +0600

    two button added on banner

---
+++ js/lets-work.js
@@ -0,0 +1,8 @@
+document.getElementById('chat').addEventListener('click', function(){
+    const mainTitle = document.getElementById('linkedin');
+    mainTitle.style.display = 'inline-block';
+    const chatBtn = document.getElementById('chat');
+    chatBtn.innerText = 'Better way is - >'
+})
+
+
---
+++ js/moving-parts.js
@@ -0,0 +1,5 @@
+function changeTheTextWith(id, newValue){
+    const mainText = document.getElementById(id);
+    mainText.innerText = newValue.innerText;
+}
+
