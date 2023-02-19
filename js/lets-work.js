fd0ca184602798563d6890181c6cf31c039adb7e
T R Methu <fmethubd@gmail.com>
2023-02-19 21:42:16 +0600

    front button added

--- js/lets-work.js
+++ js/lets-work.js
@@ -1,6 +1,8 @@
 document.getElementById('chat').addEventListener('click', function(){
     const mainTitle = document.getElementById('linkedin');
+    const emailMe = document.getElementById('email-me');
     mainTitle.style.display = 'inline-block';
+    emailMe.style.display = 'block'
     const chatBtn = document.getElementById('chat');
     chatBtn.innerText = 'Better way is - >'
 })
